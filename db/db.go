92a00e30893374bc457361d610b93a002de3fcdd
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-03-18 22:49:14 +0400

    hardcode db uri

--- db/db.go
+++ db/db.go
@@ -2,14 +2,14 @@ package db
 
 import (
 	"context"
-	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func GetClientOptions() *options.ClientOptions {
-	dburi := os.Getenv("mongodb+srv://doadmin:g3k615i2p89A7IwD@henar-db-0d7d8f8e.mongo.ondigitalocean.com/?retryWrites=true&w=majority")
+	dburi := "mongodb+srv://doadmin:g3k615i2p89A7IwD@henar-db-0d7d8f8e.mongo.ondigitalocean.com/?retryWrites=true&w=majority"
+	// dburi := os.Getenv("DB_URI")
 
 	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
 	clientOptions := options.Client().
