add26a21a07caf4bc95cb076090c5924d7914084
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-03-18 22:56:15 +0400

    remove init godotonv

--- main.go
+++ main.go
@@ -4,20 +4,13 @@ import (
 	"henar-backend/auth"
 	"henar-backend/projects"
 	"henar-backend/utils"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Some error occured. Err: %s", err)
-	}
-
 	router := mux.NewRouter()
 	router.Use(utils.RouterLoggerMiddleware)
 
