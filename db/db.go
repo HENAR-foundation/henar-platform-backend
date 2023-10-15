74ca55619bf1481fb2f9ba23a5db95d84e36e5af
Slava Safonov <stray7777@gmail.com>
2023-10-15 19:38:26 -0300

    Update db.go

--- db/db.go
+++ db/db.go
@@ -14,6 +14,7 @@ import (
 var client *mongo.Client
 
 func GetClientOptions() *options.ClientOptions {
+	//
 	dburi := os.Getenv("DB_URI")
 
 	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
